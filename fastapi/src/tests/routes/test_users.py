5225f06d1ab4575bb78b68ba1c760b2727f09c67
Tomas Ambrogi <tomi.ambro94@gmail.com>
2023-05-29 18:26:26 -0300

    tests: adding user tests

--- fastapi/src/tests/routes/test_users.py
+++ fastapi/src/tests/routes/test_users.py
@@ -1,5 +1,5 @@
 from dao import user_dao
-from utilities.jwt_token import verify_password
+from utilities.jwt_token import get_hashed_password, verify_password
 
 
 def test_me_endpoint(client, db, headers):
@@ -11,3 +11,28 @@ def test_me_endpoint(client, db, headers):
 
     response = client.get("users/me", headers=headers)
     assert response.status_code == 200
+
+
+def test_signup(client):
+    user_data = {
+        "name": "tomas2",
+        "email": "tomas2@example.com",
+        "hashed_password": get_hashed_password("1234"),
+    }
+
+    response = client.post("users/signup", json=user_data)
+    assert response.status_code == 200, response.json()
+    # try to create a new user with the same email
+    response = client.post("users/signup", json=user_data)
+    assert response.status_code == 400
+    assert response.json()["detail"] == "User with this email already exist"
+
+
+def test_incorrect_login(client):
+    login_data = {"username": "juan@example.com", "password": "wrong password"}
+    response = client.post("users/login", data=login_data)
+    assert response.status_code == 400
+
+    login_data = {"username": "tomas@example.com", "password": "wrong password"}
+    response = client.post("users/login", data=login_data)
+    assert response.status_code == 400
